9e9cc90dbc83ff1a10430ee47adf149604b68dba
Gal Zohar <gal.zohar@similarweb.com>
2020-12-25 18:49:20 +0200

    solved day 2 task

--- src/day2/day2.ts
+++ src/day2/day2.ts
@@ -1,8 +1,8 @@
 import { readFileAsStrings } from "../common/FileReader";
 
 type PasswordDetails = {
-  minReqLetterCount: number;
-  maxReqLetterCount: number;
+  policyMinimum: number;
+  policyMaximum: number;
   requiredLetter: string;
   password: string;
 };
@@ -15,31 +15,97 @@ const parsePasswords = (passwordsData: string[]): PasswordDetails[] => {
       .map((num) => Number.parseInt(num));
 
     return {
-      minReqLetterCount: numbersRange[0],
-      maxReqLetterCount: numbersRange[1],
+      policyMinimum: numbersRange[0],
+      policyMaximum: numbersRange[1],
       requiredLetter: recordDetails[1].replace(":", ""),
       password: recordDetails[2],
     };
   });
 };
 
-const isPasswordValid = ({
-  minReqLetterCount,
-  maxReqLetterCount,
-  requiredLetter,
-  password,
-}: PasswordDetails) => {
-  const targetLetterCount =
-    password.match(new RegExp(`${requiredLetter}`, "g"))?.length ?? 0;
-  return (
-    targetLetterCount >= minReqLetterCount &&
-    targetLetterCount <= maxReqLetterCount
-  );
+const getLetterLocationsInPassword = (
+  password: string,
+  policyLetter: string
+) => {
+  const regex = new RegExp(policyLetter, "gi");
+  const occouranceIndices = _searchForOccouranceIndices(password, regex, []);
+  return occouranceIndices;
+};
+
+/**
+ * Recursivley searches within a password for the given regex. accomulates all results and returns an array of all indices
+ * where the regex matches the string
+ * @param password - The input password
+ * @param regex - The regex to use for searching within the pasword. the regex should use a /g flag.
+ * @param results - accomulated results of all match indices within the password
+ */
+const _searchForOccouranceIndices = (
+  password: string,
+  regex: RegExp,
+  results: number[]
+): number[] => {
+  const match = regex.exec(password);
+
+  // In case the regex matches a result - we should add it to the previous results
+  // and continue searching for the next match in the password, by recursivley calling this function
+  // otherwise we reached the base case, and we can return all results.
+  const currResults = match
+    ? _searchForOccouranceIndices(password, regex, [...results, match.index])
+    : results;
+
+  return currResults;
 };
 
 export const day2part1 = async () => {
-  const passwordsFile = await readFileAsStrings("src/day2/input/day2part1.txt");
+  const passwordsFile = await readFileAsStrings("src/day2/input/day2.txt");
   const passwordsData = parsePasswords(passwordsFile);
-  const validPasswords = passwordsData.filter(isPasswordValid);
+
+  const validPasswords = passwordsData.filter((passData) => {
+    // Fetch all indices where the policy letter appears in the password
+    const policyLetterLocations = getLetterLocationsInPassword(
+      passData.password,
+      passData.requiredLetter
+    );
+
+    // Check the amount of matching letters, and see if its within
+    // the acceptable policy range
+    const policyLetterCount = policyLetterLocations.length;
+
+    return (
+      policyLetterCount >= passData.policyMinimum &&
+      policyLetterCount <= passData.policyMaximum
+    );
+  });
+
+  console.log(`number of valid passwords is: ${validPasswords.length}`);
+};
+
+export const day2part2 = async () => {
+  const passwordsFile = await readFileAsStrings("src/day2/input/day2.txt");
+  const passwordsData = parsePasswords(passwordsFile);
+
+  const validPasswords = passwordsData.filter((passData) => {
+    // Fetch all indices where the policy letter appears in the password
+    const policyLetterLocations = getLetterLocationsInPassword(
+      passData.password,
+      passData.requiredLetter
+    );
+
+    // Check how many indices match the provided policy locations
+    // the policy dictates that only one index can match the provided
+    // locations in order for a password to be valid
+    const locationsMatching = policyLetterLocations.filter((location) => {
+      // The policy is unaware of index zero, so we need to bump it up :(
+      const adjustedLocation = location + 1;
+
+      return (
+        adjustedLocation === passData.policyMinimum ||
+        adjustedLocation === passData.policyMaximum
+      );
+    });
+
+    return locationsMatching.length === 1;
+  });
+
   console.log(`number of valid passwords is: ${validPasswords.length}`);
 };
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { day1Part1, day1Part2 } from "./day1/day1";
-import { day2part1 } from "./day2/day2";
+import { day2part1, day2part2 } from "./day2/day2";
 
 async function runExcersie(exerciseFn: () => Promise<void>) {
     try {
@@ -9,6 +9,6 @@ async function runExcersie(exerciseFn: () => Promise<void>) {
     }
 }
 
-runExcersie(day2part1).then(() => {
+runExcersie(day2part2).then(() => {
     console.log("finished run");
 })
